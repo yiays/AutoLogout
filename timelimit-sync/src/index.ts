d8b522bba0544a5eacee94624abe3a9789863159
yiays <yiays@yiays.com>
2025-06-13 15:25:46 +1200

    Show API version as a header instead of an endpoint

--- timelimit-sync/src/index.ts
+++ timelimit-sync/src/index.ts
@@ -5,16 +5,21 @@ import { StateFetch } from "./endpoints/stateFetch";
 import { ClientAuthorize } from "./endpoints/clientAuthorize";
 
 // Constants
-const API_VERSION = 1;
+const API_VERSION = "1";
 
 // Start a Hono app
 const app = new Hono<{ Bindings: Env }>({}).basePath("");
+
+// Declare the API version in all responses
+app.use("*", async (c, next) => {
+  await next();
+  c.header("X-API-Version", API_VERSION);
+});
 
 // Setup OpenAPI registry
 const openapi = fromHono(app, { docs_url: "/" });
 
 // Register OpenAPI endpoints
-openapi.get("/api/version", (c) => c.text(API_VERSION.toString()));
 openapi.get("/api/get/:uuid", StateFetch);
 openapi.get("/api/auth/:uuid", ClientAuthorize);
 openapi.post("/api/sync/:uuid", StateSync);
