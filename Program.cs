6d1c09dfcc500f3b5050b6c1a0155a1e7c0d7eef
yiays <yiays@yiays.com>
2025-06-15 20:12:49 +1200

    Reduce clutter in the main release folder

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
 namespace AutoLogout;
+using System.Reflection;
 
 static class Program
 {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
     [STAThread]
     static void Main()
     {
+        // Help the runtime find library files that have been moved to .\Libraries\
+        AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+        {
+            var assemblyName = new AssemblyName(args.Name).Name + ".dll";
+            var probePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Libraries", assemblyName);
+            return File.Exists(probePath) ? Assembly.LoadFrom(probePath) : null;
+        };
+
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
