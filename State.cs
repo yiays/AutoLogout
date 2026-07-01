047937a33c7d13e9afa7e847de4ff058a79df95c
Yiays <yiays@yiays.com>
2026-07-01 13:31:41 +1200

    Fill out most state logic

--- State.cs
+++ State.cs
@@ -31,6 +31,11 @@ public class SyncedState : DeltaState
     public new TimeOnly bedtime = new(0, 0);
     public new TimeOnly waketime = new(0, 0);
     public new Guid? syncAuthor = null;
+    public SyncedState()
+    {
+        if(DateOnly.FromDateTime(DateTime.Today) != usageDate)
+            todayTimeLimit = dailyTimeLimit;
+    }
     public void Update(DeltaState delta)
     {
         dailyTimeLimit = delta.dailyTimeLimit ?? dailyTimeLimit;
@@ -46,35 +51,95 @@ public class SyncedState : DeltaState
 public class State
 {
     public event Action? Changed;
-    public SyncedState syncedState;
+    public SyncedState state;
     public UserIntent userIntent = UserIntent.None;
     public bool Paused = false;
     public bool graceGiven = false;
-    public int tempTimeLimit = -1; // This stores temporary overrides to the time limit. Takes priority over bedtime
-    public int bedtimeTimeLimit = -1; // This stores bedtime-related overrides to the time limit
-    private int realTimeLimit
+    public int? tempTimeLimit = null; // This stores temporary overrides to the time limit. Takes priority over bedtime
+    public int? bedtimeTimeLimit = null; // This stores bedtime-related overrides to the time limit
+    private int? RealTimeLimit
     {
-        get => tempTimeLimit != -1 ? tempTimeLimit : bedtimeTimeLimit != -1 && bedtimeTimeLimit < syncedState.todayTimeLimit ? bedtimeTimeLimit : syncedState.todayTimeLimit;
+        get
+        {
+            // First check all overrides
+            if(tempTimeLimit is not null)
+            {
+                if(tempTimeLimit == -1) return null;
+                return tempTimeLimit;
+            }
+            else if(bedtimeTimeLimit is not null)
+            {
+                // Assume bedtimeTimeLimit is always positive
+                if(bedtimeTimeLimit < state.todayTimeLimit) return bedtimeTimeLimit;
+            }
+            // Overrides don't apply, just return todayTimeLimit
+            if(state.todayTimeLimit == -1) return null;
+            return state.todayTimeLimit;
+        }
     }
-    public int remainingTime
+    public int? RemainingTime
     {
-        get => realTimeLimit == -1 ? -1 : Math.Max(realTimeLimit - syncedState.usedTime, 0);
+        get
+        {
+            var realtime = RealTimeLimit;
+            var bedtime = TimeUntilBedtime;
+            if(realtime is null) return null;
+            var usedtime = realtime - state.usedTime;
+            if(bedtime is null || usedtime < bedtime) return Math.Max((int)usedtime, 0);
+            return Math.Max((int)bedtime, 0);
+        }
+    }
+    public int? TimeUntilBedtime
+    {
+        get
+        {
+            if(state.bedtime == state.waketime) return null; // No bedtime
+            DateTime now = DateTime.Now;
+            DateTime nextBedTime = new(now.Year, now.Month, now.Day, state.bedtime.Hour, state.bedtime.Minute, 0);
+            DateTime todayWakeTime = new(now.Year, now.Month, now.Day, state.waketime.Hour, state.waketime.Minute, 0);
+            if(now > todayWakeTime && todayWakeTime > nextBedTime) nextBedTime.AddDays(1);
+            return (int)(nextBedTime - now).TotalSeconds;
+        }
     }
 
     public State()
     {
-        var loader = OS.Current.LoadState();
-        loader.Wait();
-        syncedState = loader.Result;
+        try
+        {
+            var loader = OS.Current.LoadState();
+            loader.Wait();
+            state = loader.Result;
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex);
+            state = new SyncedState();
+        }
+
+        if(state.uuid == Guid.Empty)
+        {
+            state.uuid = Guid.NewGuid();
+        }
     }
 
+    public void Tick(object? sender, EventArgs? e)
+    {
+        if(Paused) return;
+        state.usedTime++;
+        Changed?.Invoke();
+    }
+    public void TogglePause()
+    {
+        Paused = !Paused;
+        Changed?.Invoke();
+    }
     public async Task NewPassword(string password)
     {
-        syncedState.hashedPassword = BC.BCrypt.HashPassword(password);
-        await OS.Current.SaveState(syncedState);
+        state.hashedPassword = BC.BCrypt.HashPassword(password);
+        await OS.Current.SaveState(state);
     }
     public bool CheckPassword(string password)
     {
-        return BC.BCrypt.Verify(password, syncedState.hashedPassword);
+        return BC.BCrypt.Verify(password, state.hashedPassword);
     }
 }
