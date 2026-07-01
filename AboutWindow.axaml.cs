ff4ce787f7bbf2f58092cd9028fe2b05a573a3c0
Yiays <yiays@yiays.com>
2026-07-01 13:27:03 +1200

    Fix application not closing when main window is closed

--- AboutWindow.axaml.cs
+++ AboutWindow.axaml.cs
@@ -7,12 +7,5 @@ public partial class AboutWindow : Window
     public AboutWindow()
     {
         InitializeComponent();
-
-        // Hide instead of closing
-        Closing += (s, e) =>
-        {
-            Hide();
-            e.Cancel = true;
-        };
     }
 }
