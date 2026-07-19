06e3e0493862dd621c66ffceb363e795b05b7727
Yiays <yiays@yiays.com>
2026-07-20 11:41:47 +1200

    Use real version number in About screen

--- AboutWindow.axaml.cs
+++ AboutWindow.axaml.cs
@@ -1,11 +1,16 @@
 using Avalonia.Controls;
+using System.Reflection;
 
 namespace AutoLogout;
 
 public partial class AboutWindow : Window
 {
+    public string Version { get => "v"+Assembly.GetEntryAssembly()
+            ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+            ?.InformationalVersion ?? "1.0.0"; }
     public AboutWindow()
     {
         InitializeComponent();
+        DataContext = this;
     }
 }
