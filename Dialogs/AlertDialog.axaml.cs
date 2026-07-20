51d663f65e881abecf6dde567cca22c91bf5183e
Yiays <yiays@yiays.com>
2026-07-20 12:41:39 +1200

    Add AlertDialog

---
+++ Dialogs/AlertDialog.axaml.cs
@@ -0,0 +1,37 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace AutoLogout;
+
+public partial class AlertDialog : Window
+{
+    public string Text {get; set;}
+    public string Caption {get; set;}
+
+    public AlertDialog()
+    {
+        Text = "Unset";
+        Caption = "Unset";
+        InitializeComponent();
+        DataContext = this;
+
+        ConfirmButton.AttachedToVisualTree += (s,e) => ConfirmButton.Focus();
+        Activated += (s,e) => Console.Beep();
+    }
+    public AlertDialog(string text, string caption)
+    {
+        Text = text;
+        Caption = caption;
+        InitializeComponent();
+        DataContext = this;
+
+        ConfirmButton.AttachedToVisualTree += (s,e) => ConfirmButton.Focus();
+        Activated += (s,e) => Console.Beep();
+    }
+
+    private void ConfirmButton_Click(object? sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
