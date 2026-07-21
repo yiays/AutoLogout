ae8566513042258aeb9f67784fe6ac9e2f33579f
Yiays <yiays@yiays.com>
2026-07-21 12:15:25 +1200

    Improve styling of ControlPanel and AboutWindow

--- AboutWindow.axaml.cs
+++ AboutWindow.axaml.cs
@@ -5,9 +5,14 @@ namespace AutoLogout;
 
 public partial class AboutWindow : Window
 {
-    public string Version { get => "v"+Assembly.GetEntryAssembly()
-            ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-            ?.InformationalVersion ?? "1.0.0"; }
+    public string Version { get {
+            var result = Assembly.GetEntryAssembly()
+                ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion ?? "1.0.0";
+            if(result.IndexOf('+')>0)
+                result = result[..result.IndexOf('+')];
+            return "v"+result;
+        } }
     public AboutWindow()
     {
         InitializeComponent();
